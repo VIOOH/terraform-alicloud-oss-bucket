a4f134972d2a9c55d4eb56f521efa7ad9cd5a122
Marcell Baan <marcell.baan@viooh.com>
2019-07-17 14:10:07 +0100

    remove deprecated property

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ resource "alicloud_oss_bucket" "create_new_bucket" {
   acl              = "${var.acl}"
   website          = "${var.website}"
   logging          = "${var.logging}"
-  logging_isenable = "${var.logging_isenable}"
   referer_config   = "${var.referer_config}"
   lifecycle_rule   = "${var.lifecycle_rule}"
 }
